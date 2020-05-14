79db18fa1ec5336eda24bb20bd01411075e7d851
Laurențiu Nicola <lnicola@dend.ro>
2020-05-14 09:34:42 +0300

    Disable WebGL DMABUF

--- user.js
+++ user.js
@@ -30,5 +30,5 @@ user_pref("network.trr.bootstrapAddress", "1.1.1.1");
 user_pref("network.trr.mode", 0);
 user_pref("privacy.donottrackheader.enabled", true);
 user_pref("widget.wayland-dmabuf-vaapi.enabled", true);
-user_pref("widget.wayland-dmabuf-webgl.enabled", true);
+user_pref("widget.wayland-dmabuf-webgl.enabled", false);
 user_pref("widget.wayland_vsync.enabled", true);
