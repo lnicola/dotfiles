729233680628c19707a3b6ffc9bdea135fd09944
Laurențiu Nicola <lnicola@dend.ro>
2024-02-13 13:58:00 +0200

    Enable HTTPS DNS prefetch

--- user.js
+++ user.js
@@ -22,6 +22,7 @@ user_pref("media.eme.enabled", true);
 user_pref("media.ffmpeg.vaapi.enabled", true);
 user_pref("media.ffmpeg.dmabuf-textures.enabled", true);
 user_pref("media.ffvpx.enabled", false);
+user_pref("network.dns.disablePrefetchFromHTTPS", false);
 user_pref("network.IDN_show_punycode", true);
 user_pref("network.security.esni.enabled", true);
 user_pref("network.trr.bootstrapAddress", "1.1.1.1");
